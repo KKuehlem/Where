d86f2362dbca38b17963d22bdb662e1153bb7430
Konstantin Kühlem <konstantin@koeln.de>
2025-08-01 12:08:17 +0200

    Basic reflection-based identifier resolver

---
+++ src/main/java/de/kkuehlem/where/context/resolver/ObjectIdentifierResolver.java
@@ -0,0 +1,29 @@
+package de.kkuehlem.where.context.resolver;
+
+import de.kkuehlem.where.context.NoSuchIdentifierException;
+import java.lang.reflect.Field;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+
+@AllArgsConstructor
+public class ObjectIdentifierResolver implements IdentifierResolver {
+    
+    @NonNull private final Object object;
+
+    @Override
+    public Object resolveIdentifier(String name) throws NoSuchIdentifierException {
+        try {
+            Class<?> c = object.getClass();
+            Field field = c.getDeclaredField(name);
+            field.setAccessible(true);
+            return field.get(object);
+        }
+        catch (NoSuchFieldException ex) {
+            throw new NoSuchIdentifierException(name);
+        }
+        catch (IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
---
+++ src/test/java/de/kkuehlem/where/WhereObjectTest.java
@@ -0,0 +1,32 @@
+package de.kkuehlem.where;
+
+import de.kkuehlem.where.context.WhereContext;
+import de.kkuehlem.where.context.resolver.ObjectIdentifierResolver;
+import lombok.Builder;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class WhereObjectTest {
+
+    @Builder
+    private static class Person {
+
+        private final String name;
+        private final int age;
+    }
+
+    @Test
+    public void basicTest() {
+        Person p = Person.builder()
+                .name("Peter")
+                .age(42)
+                .build();
+
+        WhereContext ctx = WhereContext.builder()
+                .resolver(new ObjectIdentifierResolver(p))
+                .build();
+
+        assertTrue(Where.where("age > 40 AND age < 50", ctx));
+    }
+
+}
