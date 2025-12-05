cbd456218bdb344d366930799fc6f7fdf7191c5e
Konstantin Kühlem <konstantin@koeln.de>
2025-12-05 14:23:12 +0100

    feat: Add additional fields for the reflection-based resolver

--- src/main/java/de/kkuehlem/where/context/resolver/ObjectIdentifierResolver.java
+++ src/main/java/de/kkuehlem/where/context/resolver/ObjectIdentifierResolver.java
@@ -2,19 +2,52 @@ package de.kkuehlem.where.context.resolver;
 
 import de.kkuehlem.where.exceptions.NoSuchIdentifierException;
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 
 @AllArgsConstructor
 public class ObjectIdentifierResolver implements IdentifierResolver {
-    
+
     @NonNull private final Object object;
+    private final Map<Class<?>, Map<String, Function<?, Object>>> additionalFields = new HashMap();
 
     /**
-     * Resolves an identifier as a field of object. This can also be a nested field (e.g. person.age)
-     * @param name The name of the field, if nesting is used, fields of sub-objects are seperated with "." 
+     * Register an additional fields, which is not part of a class but should be
+     * treated as it would. If this field really exists, the additional field
+     * will override it.
+     *
+     * @param <T> the type of where the field should be added
+     * @param cls The class instance for the type
+     * @param fieldName The name of the field
+     * @param resolver A function resolving an object for the fieldName
+     * @return this, for chain calls
+     */
+    public <T> ObjectIdentifierResolver addAdditionalField(Class<T> cls, String fieldName, Function<T, Object> resolver) {
+        Map<String, Function<?, Object>> classMap = additionalFields.get(cls);
+        if (classMap == null) {
+            classMap = new HashMap<>();
+            additionalFields.put(cls, classMap);
+        }
+        classMap.put(fieldName, resolver);
+        
+        return this;
+    }
+
+    /**
+     * Resolves an identifier as a field of object. This can also be a nested
+     * field (e.g. person.age)
+     *
+     * @param name The name of the field, if nesting is used, fields of
+     *             sub-objects are seperated with "."
+     *
      * @return The value of the field of the object, can be null
-     * @throws NoSuchIdentifierException If no such field on this object or a sub-object exists
+     *
+     * @throws NoSuchIdentifierException If no such field on this object or a
+     *                                   sub-object exists
      */
     @Override
     public Object resolveIdentifier(String name) throws NoSuchIdentifierException {
@@ -26,7 +59,23 @@ public class ObjectIdentifierResolver implements IdentifierResolver {
         return o;
     }
     
-    private static Object resolve(Object object, String name) {
+    private Object resolve(Object object, String name) {
+        Objects.requireNonNull(object);
+        Objects.requireNonNull(name);
+        
+        // Check whether there is an additional field registered
+        Map<String, Function<?, Object>> classMap = additionalFields.get(object.getClass());
+        if (classMap != null) {
+            Function<Object, Object> resolver = (Function<Object, Object>) classMap.get(name);
+            if (resolver != null) {
+                return resolver.apply(object);
+            }
+        }
+        
+        return resolveWithReflection(object, name);
+    }
+
+    private static Object resolveWithReflection(Object object, String name) {
         try {
             Class<?> c = object.getClass();
             Field field = c.getDeclaredField(name);
--- src/test/java/de/kkuehlem/where/WhereObjectTest.java
+++ src/test/java/de/kkuehlem/where/WhereObjectTest.java
@@ -33,14 +33,50 @@ public class WhereObjectTest {
                         .build()
                 )
                 .build();
-        
+
         WhereContext ctx = WhereContext.builder()
                 .resolver(new ObjectIdentifierResolver(b))
                 .build();
-        
+
         assertTrue(Where.where("author.age = 42", ctx));
         assertTrue(Where.where("author.age = author.age", ctx));
         assertTrue(Where.where("author.name != title", ctx));
+    }
+
+    @Test
+    public void additionalFieldTest() {
+        Book b = Book.builder()
+                .title("The Title")
+                .author(Person.builder()
+                        .name("The Author")
+                        .age(42)
+                        .build()
+                )
+                .build();
+
+        WhereContext ctx = WhereContext.builder()
+                .resolver(new ObjectIdentifierResolver(b)
+                        .addAdditionalField(Book.class, "title", x -> "Another Title")
+                        .addAdditionalField(Book.class, "otherFieldString", x -> "Some string")
+                        .addAdditionalField(Book.class, "otherFieldNumber", x -> 42)
+                        .addAdditionalField(Person.class, "name", x -> "Another Author")
+                        .addAdditionalField(Person.class, "otherFieldNumber", x -> 123)
+                )
+                .build();
+        
+        assertTrue(Where.where("author.age = 42", ctx)); // Normal nested field
+        assertTrue(Where.where("title != 'The Title'", ctx)); // Overriden with additional field
+        assertTrue(Where.where("title = 'Another Title'", ctx)); // Overriden with additional field
+        assertTrue(Where.where("author.name = 'Another Author'", ctx)); // Overriden with additional field
+        
+        // New fields
+        assertTrue(Where.where("otherFieldString = 'Some string'", ctx));
+        assertTrue(Where.where("otherFieldNumber = 42", ctx));
+        assertTrue(Where.where("otherFieldNumber > 41", ctx));
+        assertTrue(Where.where("otherFieldNumber < 43", ctx));
+        assertTrue(Where.where("author.otherFieldNumber = 123", ctx));
+        assertTrue(Where.where("author.otherFieldNumber > 122", ctx));
+        assertTrue(Where.where("author.otherFieldNumber < 124", ctx));
     }
 
 }
